9468def875b88c1b543a6378c944fe77dc38e93f
BJ Bryant <bryantb76@gmail.com>
2020-12-04 17:09:07 -0600

    4a complete

--- lib/aoc2020.ex
+++ lib/aoc2020.ex
@@ -3,91 +3,54 @@ defmodule Aoc2020 do
   Documentation for Aoc2020.
   """
 
-  @tree "#"
+  @required_fields ~w[byr iyr eyr hgt hcl ecl pid]
 
   @doc """
   Takes the string +input+ and processes it.
 
   ## Examples
-  iex> [~w[. . # # . . . . . . .],
-  iex> ~w[# . . . # . . . # . .],
-  iex> ~w[. # . . . . # . . # .],
-  iex> ~w[. . # . # . . . # . #],
-  iex> ~w[. # . . . # # . . # .],
-  iex> ~w[. . # . # # . . . . .],
-  iex> ~w[. # . # . # . . . . #],
-  iex> ~w[. # . . . . . . . . #],
-  iex> ~w[# . # # . . . # . . .],
-  iex> ~w[# . . . # # . . . . #],
-  iex> ~w[. # . . # . . . # . #]] |> Aoc2020.run()
-  336
+  iex> "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+  iex> byr:1937 iyr:2017 cid:147 hgt:183cm
+  iex>
+  iex> iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+  iex> hcl:#cfa07d byr:1929
+  iex>
+  iex> hcl:#ae17e1 iyr:2013
+  iex> eyr:2024
+  iex> ecl:brn pid:760753108 byr:1931
+  iex> hgt:179cm
+  iex>
+  iex> hcl:#cfa07d eyr:2025 pid:166559648
+  iex> iyr:2011 ecl:brn hgt:59in
+  iex> " |> Aoc2020.run()
+  2
   """
   def run(input) do
-    length = get_length(Enum.at(input, 0))
-
-    moves = [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
-
-    moves
-    # find the tree count for each set of +moves+
-    |> Enum.reduce([], fn moves, acc -> [count_trees(input, length, moves) | acc] end)
-    # multiply the results
-    |> Enum.reduce(1, &(&1 * &2))
+    input
+    |> parse_input()
+    |> Stream.filter(&passport_valid?/1)
+    |> Enum.count()
   end
 
-  def parse_line(line) when is_binary(line), do: String.trim(line) |> String.graphemes()
-  def parse_line(line), do: line
+  def parse_input(input) when is_binary(input) do
+    {:ok, input} = input |> StringIO.open()
 
-  def get_length(line) when is_binary(line), do: String.trim(line) |> String.length()
-  def get_length(line), do: length(line)
-
-  def tree?(line, {num, acc = %{move: {x, y}, i: i}}) do
-    # increment the slope index, +i+
-    acc = Map.put(acc, :i, acc.i + 1)
-
-    case y do
-      # when +y+ is 1, we can use the same formula from 3a
-      1 ->
-        {num + increment_amount(line, x, i, acc.length), acc}
-
-      # otherwise, we can only check even rows
-      _ ->
-        cond do
-          # our very first row doesn't need to be counted, even though it's even
-          y > 0 && i == 0 ->
-            {num, acc}
-
-          # if this is an even row, check for a tree
-          rem(i, y) == 0 ->
-            # pass both +x+ and +y+ so we can determine the current index on the slope
-            {num + increment_amount(line, {x, y}, i, acc.length), acc}
-
-          # if it's not even and it's not the very first row, it must be odd. don't count it
-          true ->
-            {num, acc}
-        end
-    end
+    input
+    |> IO.binstream(:line)
+    |> parse_input()
   end
 
-  def count_trees(stream, slope_length, {x, y}) do
-    stream
-    # clean up the line
-    |> Stream.map(&parse_line/1)
-    # scan the stream count the number of trees
-    |> Stream.scan({0, %{i: 0, length: slope_length, move: {x, y}}}, &tree?/2)
-    # all we need is the count, not the accumulator
-    |> Stream.map(fn {i, _} -> i end)
-    # we want the last (highest) number
-    |> Enum.reverse()
-    |> hd()
+  def parse_input(input) do
+    input
+    |> Stream.chunk_by(&String.match?(&1, ~r/^\n$/))
+    |> Stream.map(&Stream.map(&1, fn line -> String.trim(line) end))
+    |> Stream.map(&Enum.join(&1, " "))
+    |> Stream.filter(&(String.length(&1) > 0))
   end
 
-  # When +y+ isn't 1, we need to divide the current +index+ by +y+
-  # to get the current position.
-  def increment_amount(slope, {x, y}, index, slope_length) do
-    (Enum.at(slope, rem(div(index, y) * x, slope_length)) === @tree && 1) || 0
-  end
-
-  def increment_amount(slope, moves, index, slope_length) do
-    (Enum.at(slope, rem(index * moves, slope_length)) === @tree && 1) || 0
+  def passport_valid?(line) do
+    @required_fields
+    |> Stream.map(&String.match?(line, ~r/#{&1}/))
+    |> Enum.all?()
   end
 end
