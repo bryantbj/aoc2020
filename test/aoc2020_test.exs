8d94efd0a7cd72c38062802924f197afa81f2cfa
BJ Bryant <bbryant@covermymeds.com>
2021-12-02 19:29:58 -0600

    7a complete!
    Phew. Wanted to use a graphing library, as I never have. Took a bit for me to figure it out.

--- test/aoc2020_test.exs
+++ test/aoc2020_test.exs
@@ -2,8 +2,7 @@ defmodule Aoc2020Test do
   use ExUnit.Case, async: true
   doctest Aoc2020
 
-  @tag :skip
   test "input works" do
-    File.stream!("data/input.txt") |> Aoc2020.run() |> IO.inspect(label: "answer", limit: 2000)
+    Aoc2020.run()
   end
 end
